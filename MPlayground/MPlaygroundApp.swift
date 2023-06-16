90c1468188ef9ce5a4791997189f47894e40629b
Fan Wu <fanwu8184@gmail.com>
2023-06-16 10:24:28 +1000

    Initial Commit

---
+++ MPlayground/MPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPlaygroundApp.swift
+//  MPlayground
+//
+//  Created by Fan Wu on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
